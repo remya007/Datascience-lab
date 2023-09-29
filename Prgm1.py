c7672be9599f98481351ede65f828080e2cc212e
Pournami-Prasad27 <117921519+Pournami-Prasad27@users.noreply.github.com>
2023-09-29 14:29:25 +0530

    Add files via upload

---
+++ Prgm1.py
@@ -0,0 +1,11 @@
+print("demo of basic data types:Numbers")
+x=3
+y=25
+print("x=",x)
+print("y",y)
+print("datatatype of varible x:",type(x))
+print("datatype of varible y:",type(y))
+print("Addition",x+y)
+print("subtraction",x-y)
+print("Exponents",x**2)
+print("Multiplication",x*2)
