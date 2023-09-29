1154a3d28e2397ee00405c7af2e3750cf769fa83
Pournami-Prasad27 <117921519+Pournami-Prasad27@users.noreply.github.com>
2023-09-29 14:59:11 +0530

    Add files via upload

---
+++ prgm3.py
@@ -0,0 +1,13 @@
+print("Demo of basic datatypes:String")
+s="hello"
+t="world"
+print("string1",s)
+print("string2",t)
+d=s+""+t
+print("String concatenation:",d)
+print("capitalize:",d.capitalize())
+print("Right justify a string:",s.rjust(7))
+print("string at center :",s.center(7))
+print("after replacing l with ell:",s.replace("l","(ell)"))
+print("string striping leading and trailling white spcae:",'word'.strip())
+print("converted to upercase:",s.upper())
