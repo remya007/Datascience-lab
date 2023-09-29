1062ea090a1ffb31a37a21536da7cbc3325c7ccd
Pournami-Prasad27 <117921519+Pournami-Prasad27@users.noreply.github.com>
2023-09-29 14:39:25 +0530

    Add files via upload

---
+++ prgm2.py
@@ -0,0 +1,11 @@
+print("Demo of basic datatypes:Boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print("Datatype of vrible t:",type(t))
+print("Datatype of vrible f:",type(f))
+print("Logical AND operation:",t and f)
+print("Logical OR operation:",t or f)
+print("logical NOT operation",not t)
+print("logical XOR operation",t!=f)
